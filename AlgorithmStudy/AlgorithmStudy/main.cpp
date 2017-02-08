7b6ea3e297fa12755922360c85e00e68e14957c0
wang_wonderful <wang_wonderful@foxmail.com>
2017-02-08 11:16:41 +0800

    add

--- AlgorithmStudy/AlgorithmStudy/main.cpp
+++ AlgorithmStudy/AlgorithmStudy/main.cpp
@@ -14,16 +14,26 @@ void insertSort2(int a[], int n);
 
 void shellSort(int array[], int length);
 
+void mergeSort(int array[], int length);
+void p_mergeSort(int array[],int l,int r);
+void mergeArray(int a[],int first, int mid, int last);
+
 void printArray(int array[], int length);
+
+int *g_array = nullptr;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     int array[] = {99,4,35,12,78,90,33,46,59,25};
     int length = sizeof(array) / sizeof(array[0]);
     
+    g_array = new int[512];
+    
 //    insertSort1(array, length);
 //    insertSort2(array, length);
-    shellSort(array,length);
+//    shellSort(array,length);
+    
+    mergeSort(array, length);
     
     printArray(array, length);
     
@@ -82,6 +92,48 @@ void shellSort(int array[], int length){
                 array[j] = temp;
             }
         }
+    }
+}
+
+void mergeSort(int array[], int length){
+    p_mergeSort(array, 0, length - 1);
+}
+
+void p_mergeSort(int array[],int l,int r){
+    if(l < r){
+        int mid = (l + r) / 2;
+        p_mergeSort(array, l, mid);
+        p_mergeSort(array, mid + 1, r);
+        mergeArray(array, l, mid, r);
+    }
+}
+
+void mergeArray(int a[],int first, int mid, int last){
+    int i = first;
+    int j = mid + 1;
+    
+    int n = mid;
+    int m = last;
+    int k = 0;
+    
+    while (i <= n && j <= m) {
+        if(a[i] < a[j]){
+            g_array[k++] = a[i++];
+        }else{
+            g_array[k++] = a[j++];
+        }
+    }
+    
+    while (i <= n) {
+        g_array[k++] = a[i++];
+    }
+    
+    while (j <= m) {
+        g_array[k++] = a[j++];
+    }
+    
+    for (int i = 0; i < k; ++i) {
+        a[first + i] = g_array[i];
     }
 }
 
