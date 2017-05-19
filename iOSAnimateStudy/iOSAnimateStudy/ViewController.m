d2665f6bc0e72dd19f0dc1d7c64cabf5dac4a32b
wang_wonderful <wang_wonderful@foxmail.com>
2017-05-19 17:04:58 +0800

    add

--- iOSAnimateStudy/iOSAnimateStudy/ViewController.m
+++ iOSAnimateStudy/iOSAnimateStudy/ViewController.m
@@ -26,22 +26,24 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     
-    CALayer *blueLayer = [CALayer layer];
-    blueLayer.frame = CGRectMake(50, 50, 100, 100);
-    blueLayer.backgroundColor = [UIColor blueColor].CGColor;
+//    CALayer *blueLayer = [CALayer layer];
+//    blueLayer.frame = CGRectMake(50, 50, 100, 100);
+//    blueLayer.backgroundColor = [UIColor blueColor].CGColor;
+//    
+//    blueLayer.delegate = self;
+//    blueLayer.contentsScale = [UIScreen mainScreen].scale;
+//    
+//    blueLayer.masksToBounds = NO;
+//
+//    [self.layerView.layer addSublayer:blueLayer];
+//    
+//    [blueLayer display];
+//    self.blueLayer = blueLayer;
+//    
+//    self.blueLayer.borderColor = [UIColor blackColor].CGColor;
+//    self.blueLayer.borderWidth = 5;
     
-    blueLayer.delegate = self;
-    blueLayer.contentsScale = [UIScreen mainScreen].scale;
     
-    blueLayer.masksToBounds = NO;
-
-    [self.layerView.layer addSublayer:blueLayer];
-    
-    [blueLayer display];
-    self.blueLayer = blueLayer;
-    
-    self.blueLayer.borderColor = [UIColor blackColor].CGColor;
-    self.blueLayer.borderWidth = 5;
     
 //    UIImage *image1 = [UIImage imageNamed:@"Gift@2x.png"];
 //    
@@ -51,12 +53,12 @@
 //    self.imageView2.image = imag2;
 //    
 //    
-//    UIImage *image = [UIImage imageNamed:@"2.jpeg"];
-//    self.layerView.layer.contentsGravity = kCAGravityResizeAspect;
-//    self.layerView.layer.contentsScale = image.scale;
-//    NSLog(@"image.scale = %@",@(image.scale));
+    UIImage *image = [UIImage imageNamed:@"2.jpeg"];
+    self.layerView.layer.contentsGravity = kCAGravityResizeAspect;
+    self.layerView.layer.contentsScale = image.scale;
+    NSLog(@"image.scale = %@",@(image.scale));
 //    self.layerView.layer.contentsRect = CGRectMake(0, 0, 0.5, 0.5);
-//    self.layerView.layer.contents = (__bridge id)image.CGImage;
+    self.layerView.layer.contents = (__bridge id)image.CGImage;
 }
 
 //- (void)drawLayer:(CALayer *)layer inContext:(CGContextRef)ctx{
@@ -90,6 +92,25 @@
     
     
     
+    // 形变
+//    CGAffineTransform t = CGAffineTransformIdentity;
+//    
+//    t = CGAffineTransformScale(t, 0.5, 0.5);
+//    t = CGAffineTransformRotate(t, M_PI_4);
+//    t = CGAffineTransformTranslate(t, 0, 200);
+//    
+//    self.layerView.layer.affineTransform = t;
+    
+    
+    // 3D 效果，近大远小
+    CATransform3D tt = CATransform3DIdentity;
+    // 要先写，否则没效果
+    tt.m34 = -1.0 / 500.0;
+    tt = CATransform3DRotate(tt, M_PI_4, 0, 1, 0);
+    
+    
+    
+    self.layerView.layer.transform = tt;
 }
 
 
